be239eeb989b760165c16e01419d323ae4264d7b
shhridoy <shhridoy.cste@gmail.com>
2017-06-07 15:34:42 +0600

    Initial commit
    :) :)

---
+++ 2d Racing game/Assets/scripts/carController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour {
+
+	public float carSpeed;
+	Vector3 position;
+	public float maxPos = 2.5f;
+	public float minPos = -2.5f;
+
+	public uiManager ui;
+	public AudioManager am;
+	bool currentPlatformAndroid = false;
+
+	Rigidbody2D rb;
+
+	void Awake(){
+		
+		rb = GetComponent<Rigidbody2D> ();
+
+        #if UNITY_ANDROID
+		    currentPlatformAndroid = true;
+		#else
+		    currentPlatformAndroid = false;
+		#endif
+
+		am.carSound.Play();
+	}
+
+	// Use this for initialization
+	void Start () {
+		//ui = GetComponent<uiManager> ();
+	
+		position = transform.position;
+
+		if (currentPlatformAndroid) {
+			Debug.Log ("Android");
+		} else {
+			Debug.Log ("Windows");
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentPlatformAndroid) {
+			// android specific codes
+			//TouchMove ();
+			AccelerometerMove ();
+		} 
+
+		else {
+			position.x += Input.GetAxis ("Horizontal") * carSpeed * Time.deltaTime;
+
+			position.x = Mathf.Clamp (position.x, minPos, maxPos);
+
+			transform.position = position;
+		}
+
+		position = transform.position;
+		position.x = Mathf.Clamp (position.x, minPos, maxPos);
+		transform.position = position;
+	}
+
+	// this method is called when two game object get collided
+	void OnCollisionEnter2D (Collision2D col){
+		if (col.gameObject.tag == "Enemy Car") {
+			//Destroy (gameObject);
+			gameObject.SetActive (false);
+			ui.gameOverActivated ();
+			am.carSound.Stop ();
+		}
+	}
+
+	void AccelerometerMove(){
+		float x = Input.acceleration.x;
+
+		if (x < -0.1f) {
+			MoveLeft ();
+		} else if (x > 0.1f) {
+			MoveRight ();
+		} else {
+			SetVelocityZero ();
+		}
+
+	}
+
+	void TouchMove(){
+		if (Input.touchCount > 0) {
+			Touch touch = Input.GetTouch (0);
+
+			float middle = Screen.width / 2;
+
+			if (touch.position.x < middle && touch.phase == TouchPhase.Began) {
+				MoveLeft ();
+			} 
+			else if (touch.position.x > middle && touch.phase == TouchPhase.Began) {
+				MoveRight ();
+			} 
+		}
+
+		else {
+			SetVelocityZero ();
+		}
+	}
+
+	public void MoveLeft(){
+		rb.velocity = new Vector2 (-carSpeed, 0);
+	}
+
+	public void MoveRight(){
+		rb.velocity = new Vector2 (carSpeed, 0);
+	}
+
+	public void SetVelocityZero(){
+		rb.velocity = Vector2.zero;
+	}
+}
---
+++ 2d Racing game/Assets/uiManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiManager : MonoBehaviour {
+
+	public Button[] buttons;
+	bool gameOver;
+	public Text scoreText;
+	public AudioManager am;
+	int score;
+
+	// Use this for initialization
+	void Start () {
+		gameOver = false;
+		score = 0;
+		InvokeRepeating ("scoreUpdate", 1.0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "Score: " + score;
+	}
+
+	void scoreUpdate(){
+		if (gameOver == false) {
+			score += 1;
+		}
+
+	}
+
+	public void gameOverActivated(){
+		gameOver = true;
+		foreach (Button button in buttons) {
+			button.gameObject.SetActive(true);
+		}
+	}
+
+	public void Play(){
+		Application.LoadLevel ("level1");
+	}
+
+	public void Pause(){
+		if (Time.timeScale == 1) {
+			Time.timeScale = 0;
+			am.carSound.Stop ();
+		} 
+		else if (Time.timeScale == 0) {
+			Time.timeScale = 1;
+			am.carSound.Play ();
+		}
+	}
+
+	/*public void Replay(){
+		Application.LoadLevel (Application.loadedLevel);
+	}*/
+
+	public void Menu(){
+		Application.LoadLevel ("menuScene");
+	}
+
+	public void Exit(){
+		Application.Quit ();
+	}
+}
